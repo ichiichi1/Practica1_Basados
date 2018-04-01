cf2c8ad93f4f5b72ee31261fa2faa99d309aa571
Felipiuks <35807866+Felipiuks@users.noreply.github.com>
2018-04-01 06:26:18 -0500

    Create globalfunctions.h

---
+++ globalfunctions.h
@@ -0,0 +1,14 @@
+/*
+ * GlobalFunctions.h
+ *
+ *  Created on: Apr 1, 2018
+ *      Author: Felipe Rivera
+ */
+
+#ifndef GLOBALFUNCTIONS_H_
+#define GLOBALFUNCTIONS_H_
+#include "fsl_port.h"
+
+void delay(uint16_t);
+
+#endif /* GLOBALFUNCTIONS_H_ */
